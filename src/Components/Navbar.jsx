76daf0e664bf2dcc6e11248b779cba2dadd95d87
mac <mac@macs-MacBook-Pro.local>
2024-11-02 20:51:55 +0700

    ofcanvas

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import OffCanvas from './OffCanvas'; // Pastikan Anda telah membuat komponen OffCanvas
+import OffCanvas from './OffCanvas'; 
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
